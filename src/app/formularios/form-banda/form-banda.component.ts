81ccc2e96eaafb93e5c6f93903e6bdb55764b27f
dovejero <d.ovejerovargas@gmail.com>
2019-06-07 00:07:39 +0200

    imagenes en formulario para logo

--- src/app/formularios/form-banda/form-banda.component.ts
+++ src/app/formularios/form-banda/form-banda.component.ts
@@ -11,6 +11,10 @@ import { finalize } from 'rxjs/operators'
 })
 export class FormBandaComponent implements OnInit {
 
+  imagePath;
+  imgURL: any;
+  message: string;
+
   formulario: FormGroup;
   control: boolean;
   visible: string[];
@@ -67,10 +71,24 @@ export class FormBandaComponent implements OnInit {
   ngOnInit() {
   }
 
-  onChangeLogo(e) {
+  onChangeLogo(e, files) {
     console.log(e)
+    if (files.length === 0)
+      return;
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    }
     this.logoO = e.target.files[0];
   }
+
   onChangeImg(e) {
     console.log(e)
     this.imagenO = e.target.files[0];
