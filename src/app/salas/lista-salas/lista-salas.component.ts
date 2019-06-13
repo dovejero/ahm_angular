51021fbb254a92107b92e4d4381ea544ccaf190d
dovejero <d.ovejerovargas@gmail.com>
2019-06-13 15:32:57 +0200

    filtrado salas

--- src/app/salas/lista-salas/lista-salas.component.ts
+++ src/app/salas/lista-salas/lista-salas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { BandasService } from '../../servicios/bandas.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-salas',
@@ -8,15 +10,54 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ListaSalasComponent implements OnInit {
   parametro: number;
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private bandasService: BandasService) {
 
   }
-
+  formulario: FormGroup;
+  provinciaArray: any;
+  localidadArray: any;
   ngOnInit() {
     // this.activatedRoute.parent.params.subscribe(params => {
     //   console.log('PARAMS PADRE: ', params.id);
     //   this.parametro = params.id;
     // })
+    this.provinciaArray = [];
+    this.localidadArray = [];
+    this.formulario = new FormGroup({
+      nombre: new FormControl('', [
+      ]),
+      aforo: new FormControl(0, [
+      ]),
+      provincia: new FormControl('', [
+      ]),
+      localidad: new FormControl('', [
+      ])
+    })
+    this.listaProvicias()
   }
 
+  listaProvicias() {
+    this.bandasService.getProvincias().then((res) => {
+      this.provinciaArray = res;
+
+    }).catch((err) => {
+
+    });
+  }
+
+  datosLocalidad(plocalidad) {
+    console.log(plocalidad.target.value)
+    let arrayLocalidades = plocalidad.target.value.split(',')
+    this.bandasService.getLocalidades(arrayLocalidades[0]).then((res) => {
+      this.localidadArray = res;
+
+    }).catch((err) => {
+
+    });
+    this.formulario.value.provincia = arrayLocalidades[1];
+  }
+  datosLatLng(plocalidad) {
+    let arrayLocalidades = plocalidad.target.value.split(',')
+    this.formulario.value.localidad = arrayLocalidades[0];
+  }
 }
