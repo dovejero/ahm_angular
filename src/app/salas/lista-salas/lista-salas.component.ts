f971c6f4938500cd4607b6c723cd9e4755c99c69
dovejero <d.ovejerovargas@gmail.com>
2019-06-13 16:24:09 +0200

    filtro salas

--- src/app/salas/lista-salas/lista-salas.component.ts
+++ src/app/salas/lista-salas/lista-salas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BandasService } from '../../servicios/bandas.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { SalasService } from '../../servicios/salas.service';
 
 @Component({
   selector: 'app-lista-salas',
@@ -10,12 +11,13 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ListaSalasComponent implements OnInit {
   parametro: number;
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private bandasService: BandasService) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private bandasService: BandasService, private salasService: SalasService) {
 
   }
   formulario: FormGroup;
   provinciaArray: any;
   localidadArray: any;
+  listaFiltrada: any;
   ngOnInit() {
     // this.activatedRoute.parent.params.subscribe(params => {
     //   console.log('PARAMS PADRE: ', params.id);
@@ -34,6 +36,11 @@ export class ListaSalasComponent implements OnInit {
       ])
     })
     this.listaProvicias()
+    this.salasService.getAllSalas().then((res) => {
+      this.listaFiltrada = res;
+    }).catch((err) => {
+
+    })
   }
 
   listaProvicias() {
