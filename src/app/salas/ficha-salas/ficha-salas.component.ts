2c532fd54eafc80957bb2a9b1c33c25ce85aa300
dovejero <d.ovejerovargas@gmail.com>
2019-06-16 16:18:51 +0200

    traducciones e interface

--- src/app/salas/ficha-salas/ficha-salas.component.ts
+++ src/app/salas/ficha-salas/ficha-salas.component.ts
@@ -10,6 +10,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class FichaSalasComponent implements OnInit {
   datosSala: any;
   latlng: any;
+  redesSociales: any;
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private salasService: SalasService) { }
 
   ngOnInit() {
@@ -23,6 +24,7 @@ export class FichaSalasComponent implements OnInit {
       this.datosSala = res;
       this.latlng = { lat: this.datosSala.lat, lng: this.datosSala.lng };
       console.log('DATOS SALAAAAAA: ', this.datosSala)
+      this.redesSociales = this.datosSala['redes'].split(',');
     }).catch((err) => {
       this.router.navigate([`/eventos`]);
     });
